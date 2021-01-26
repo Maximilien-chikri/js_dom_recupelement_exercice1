2b9cb158c1baf4954589dfef356a6ecd34e4045c
Maximilien <maximilienchikri@gmail.com>
2021-01-26 10:35:41 +0100

    fin exo

---
+++ dom_recuperation_elements_part1_exercice1/main.js
@@ -0,0 +1,18 @@
+// 1
+let h1 = document.getElementsByTagName("h1");
+console.log(h1);
+// 2
+let h3 = document.getElementsByTagName("h3");
+console.log(h3);
+let h31 = document.getElementsByTagName("h3")[0];
+let h32 = document.getElementsByTagName("h3")[1];
+let h33 = document.getElementsByTagName("h3")[2];
+console.log(h31);
+console.log(h32);
+console.log(h33);
+// 3
+let par = document.getElementsByTagName("div");
+console.log(par);
+// 4
+let li = document.getElementsByTagName("li")[2];
+console.log(li);
